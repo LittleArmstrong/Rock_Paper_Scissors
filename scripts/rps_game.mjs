cfaa75db206be68ec0028b12f600d93eefbeb30f
LittleArmstrong <b.mungan@hotmail.de>
2022-04-09 23:24:32 +0200

    change algorithm for comp choice and round result, end match at 5 points
    Changed comp choice to a random choice and the round result is calculated from both choices. Whoever hast 5 points first wins.

--- scripts/rps_game.mjs
+++ scripts/rps_game.mjs
@@ -8,8 +8,8 @@ const constants = {
    PAPER: 0,
    ROCK: 1,
    SCISSORS: 2,
-   ROUND_RESULT_LIST: ["LOSS", "DRAW", "WIN"],
-   MAX_SCORE_ROUND: 2,
+   ROUND_RESULT_LIST: ["DRAW", "LOSS", "WIN"],
+   MAX_MATCH_SCORE: 5,
 };
 
 const round_state = {
@@ -44,12 +44,7 @@ function show_comp_choosing(resolve) {
 }
 
 function get_comp_choice() {
-   round_state.comp_choice =
-      (constants.N_CHOICES + //so that result is not negative
-         round_state.round_result +
-         round_state.player_choice -
-         1) %
-      constants.N_CHOICES;
+   round_state.comp_choice = get_random_int(0, constants.N_CHOICES - 1);
 }
 
 function show_comp_choice(choice, resolve = null) {
@@ -60,7 +55,10 @@ function show_comp_choice(choice, resolve = null) {
 }
 
 function get_round_result() {
-   round_state.round_result = get_random_int(0, constants.N_CHOICES - 1);
+   //choice order is so that the value at index i wins against the one at index i+1 (reset to 0 if over limit)
+   //that way this algorithm gets the following values in this case: Win = 2, Loss = 1 and Draw = 0 (see round_result_list)
+   round_state.round_result =
+      (round_state.player_choice + 2 * round_state.comp_choice) % constants.N_CHOICES;
 }
 
 function show_round_result(resolve = null) {
@@ -71,8 +69,9 @@ function show_round_result(resolve = null) {
 }
 
 function update_score() {
-   match_state.player_score += round_state.round_result;
-   match_state.comp_score += constants.MAX_SCORE_ROUND - round_state.round_result;
+   const player_round_score = Math.floor(round_state.round_result / (constants.N_CHOICES - 1)); //get 1 Point if won else 0
+   match_state.player_score += player_round_score;
+   match_state.comp_score += (round_state.round_result + player_round_score) % constants.N_CHOICES; //opposite for comp
 }
 
 function show_score(resolve = null) {
@@ -98,13 +97,13 @@ function reset_round() {
 async function play_one_round() {
    reset_round();
    show_player_choice();
-   get_round_result(); //randomly decide the result of the match
-   get_comp_choice(); //get comp choice based on result and player choice
 
    // show comp choosing
    await new Promise((resolve) => {
       show_comp_choosing(resolve);
    });
+
+   get_comp_choice();
 
    // show comp choice
    await new Promise((resolve) => {
@@ -116,6 +115,8 @@ async function play_one_round() {
       );
    });
 
+   get_round_result();
+
    //show round result
    await new Promise((resolve) => {
       setTimeout(show_round_result, rps_game.settings.show_round_result_time, resolve);
@@ -123,23 +124,35 @@ async function play_one_round() {
 
    //update and show actual score
    update_score();
+
    await new Promise((resolve) => {
       setTimeout(show_score, rps_game.settings.show_score_time, resolve);
    });
 
-   //mark end of this round
-   round_state.is_new_round = true;
+   if (match_state.player_score >= constants.MAX_MATCH_SCORE) {
+      setTimeout(() => {
+         rps_game.outputs.game_result.textContent = "YOU WON!";
+      }, rps_game.settings.show_match_result_time);
+   } else if (match_state.comp_score >= constants.MAX_MATCH_SCORE) {
+      setTimeout(() => {
+         rps_game.outputs.game_result.textContent = "YOU LOST!";
+      }, rps_game.settings.show_match_result_time);
+   } else {
+      //mark end of this round
+      round_state.is_new_round = true;
+   }
 }
 
 export const rps_game = {
    init(inputs, outputs, new_settings = null) {
-      this.inputs = inputs;
-      this.outputs = outputs;
+      this.inputs = inputs; //rock, paper, scissors
+      this.outputs = outputs; //  player_choice, player_score, comp_choice, comp_score, game_result
       this.settings = {
          //default settings
          comp_choosing_time: 1000,
          show_comp_choice_time: 500,
          show_round_result_time: 500,
+         show_match_result_time: 500,
          show_score_time: 500,
       };
 
